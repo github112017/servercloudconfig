8875444cd280318ed12311db69783ac74634f968
github112017 <33855412+github112017@users.noreply.github.com>
2019-10-18 11:12:10 +0530

    Experiment

--- config-client/src/main/java/com/profesorp/configclient/ConfigServiceController.java
+++ config-client/src/main/java/com/profesorp/configclient/ConfigServiceController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.profesorp.configclient.bean.BeanConfiguration;
 
+
 @RefreshScope
 @RestController
 public class ConfigServiceController {
